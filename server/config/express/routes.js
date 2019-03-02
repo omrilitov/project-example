54b956a609497bf5cdd1ee965022bf118e109119
Omri Litov <omrilitov@gmail.com>
2019-03-03 01:25:57 +0200

    fix 404 and todo

--- server/config/express/routes.js
+++ server/config/express/routes.js
@@ -1,8 +1,6 @@
-const {join} = require('path');
 const createError = require('http-errors');
 
 const userRoute = require('../../api/user');
-
 const authRoute = require('../../auth');
 
 module.exports = app => {
@@ -10,11 +8,7 @@ module.exports = app => {
 
   app.use('/auth', authRoute);
 
-  app.route('/:url(api|auth)/*')
-    .get((req, res, next) => {
-      next(createError(404));
-    });
-
-  app.route('/*')
-    .get((req, res) => res.sendFile(join(__dirname, '..', '..', '..', 'client', 'index.html')));
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
 };
